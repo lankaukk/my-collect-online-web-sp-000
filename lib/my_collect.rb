7422adb32a6958c02c7526e9598907b92eafb7b8
McKayla Lankau <lankaukk@yahoo.com>
2020-08-07 22:33:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < array.length
-    collection << yield(array[i])
+    new_collection << yield(array[i])
     i += 1
   end
   new_collection
